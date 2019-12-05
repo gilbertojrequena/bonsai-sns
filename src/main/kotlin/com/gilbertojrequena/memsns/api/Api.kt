69e227c2272c3e1a42ad489d18158e0ad0add518
Gilberto J Requena <gh.gilbertojrequena@fastmail.com>
2019-12-05 20:07:42 +0100

    Remove test endpoint

--- src/main/kotlin/com/gilbertojrequena/memsns/api/Api.kt
+++ src/main/kotlin/com/gilbertojrequena/memsns/api/Api.kt
@@ -3,13 +3,10 @@ package com.gilbertojrequena.memsns.api
 import com.gilbertojrequena.memsns.api.action.RequestHandler
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
-import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.route
-import mu.KotlinLogging
-import java.util.*
 
 fun HttpStatusCode.isRetriable(): Boolean {
     return this.value > 499
@@ -20,24 +17,12 @@ fun Routing.api(requestHandler: RequestHandler) {
 }
 
 private fun Routing.snsApi(requestHandler: RequestHandler) {
-    val random = Random(523523L)
     route("") {
         get {
             requestHandler.processRequest(call)
         }
         post {
             requestHandler.processRequest(call)
-        }
-    }
-    route("/action") {
-        post {
-            val log = KotlinLogging.logger { }
-            log.debug { "Message for action received" }
-            if (random.nextInt(5) == 4) {
-                call.respond(HttpStatusCode.ServiceUnavailable, "")
-            } else {
-                call.respond(HttpStatusCode.OK, "")
-            }
         }
     }
 }
